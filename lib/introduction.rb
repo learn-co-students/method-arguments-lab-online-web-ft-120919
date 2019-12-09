22ba8c793eed615b10206ecfdbe5b89f1215b953
james hall <halljames631@gmail.com>
2019-12-09 11:07:05 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
